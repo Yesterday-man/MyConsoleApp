58b79d50ea77d809898ab0c36482438617bdad3e
Yesterday-man <sakai.eiichiro.4649@gmail.com>
2023-09-05 17:52:58 +0900

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+Console.WriteLine("引数を入力してください.");
+
+string var = Console.ReadLine();
+
+string[] GGG; ;
+
+GGG = var.Split(' ');
+
+int a = Int32.Parse( GGG[0]);
+int b = Int32.Parse( GGG[1]);
+
+int anser = a + b;
+
+Console.WriteLine(anser);
+
+Console.Read();
