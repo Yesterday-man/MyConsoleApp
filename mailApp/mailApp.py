05f38e8b35efeb628a86bd34dbe297eeaa40e393
Yesterday-man <sakai.eiichiro.4649@gmail.com>
2023-09-08 15:34:15 +0900

    aho

--- mailApp/mailApp.py
+++ mailApp/mailApp.py
@@ -1,4 +1,7 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 sender = "everyday.no.everyday.4649@gmail.com"
 receiver = "everyday.no.everyday.4649@gmail.com"
@@ -9,6 +12,21 @@ To: {receiver}
 From: {sender}
 
 Build is finshed"""
+
+
+msg = MIMEMultipart()
+
+zip_filename = "publish-output.zip"
+zip_attachment = open("../publish-output.zip","rb")
+
+attachment = MIMEBase('application','octet-stream')
+
+attachment.set_payload((zip_attachment).read())
+
+encoders.encode_base64(attachment)
+attachment.add_header('Content_Disposition',"attachment; filename= %s" % zip_filename)
+
+msg.attach(attachment)
 
 with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
     server.login("f67a47abcc6cc4", "ce1f9486228ef4")
