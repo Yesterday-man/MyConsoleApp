d5c9ce1c8b15d4ba1f115603d0456d0feb0708af
Yesterday-man <sakai.eiichiro.4649@gmail.com>
2023-09-08 14:47:01 +0900

    wow

--- mailApp/mailApp.py
+++ mailApp/mailApp.py
@@ -4,11 +4,11 @@ sender = "everyday.no.everyday.4649@gmail.com"
 receiver = "everyday.no.everyday.4649@gmail.com"
 
 message = f"""\
-Subject: こんにちは
+Subject: Hellow Mr and Ms
 To: {receiver}
 From: {sender}
 
-ビルドはうまくいきましたか？"""
+Build is finshed"""
 
 with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
     server.login("f67a47abcc6cc4", "ce1f9486228ef4")
